e7c995cbd5556b12c235b902345fa86ac0e8de55
David Malinowski <dev@itsdave.de>
2020-01-31 02:39:06 +0100

    import Creditors and assign Accounts

--- somefunctions/somefunctions/doctype/some_functions/some_functions.py
+++ somefunctions/somefunctions/doctype/some_functions/some_functions.py
@@ -8,6 +8,7 @@ from frappe.utils import file_manager
 import csv
 import re
 from frappe.model.document import Document
+from pprint import pprint
 
 class SomeFunctions(Document):
 
@@ -19,7 +20,6 @@ class SomeFunctions(Document):
 			cfe = 0
 			for row in csv_reader:
 				cff = cff + 1
-				log.append(str(row) + "<br>")
 
 				regex = re.compile('^\d{5}$')
 				if None not in (regex.match(row[0]), regex.match(row[2])):
@@ -59,5 +59,138 @@ class SomeFunctions(Document):
 						
 					
 		print("stats: cff: " + str(cff) + " cfe:  " + str(cfe))
+
+	
+	
+	def dryrun(self):
+		log =[]
+		with open(frappe.utils.file_manager.get_file_path(self.suppliers_csv)) as csv_file:
+			csv_reader = csv.reader(csv_file, delimiter=';')
+			cff = 0 #lines processed
+			cfe = 0 #existing suppliers
+			list_cfe = []
+			cfn = 0 #new suppliers
+			list_cfn = []
+			list_all = []
+			accounts = []
+			for row in csv_reader:
+				list_all.append({"name": row[0], "account": row[1]})
+				cff += 1
+				accounts.append(int(row[1]))
+				try:
+					supplier_list = frappe.get_list("Supplier", filters={"supplier_name": row[0]} )
+					if supplier_list:
+						if len(supplier_list) == 1:
+							cfe += 1
+							print("found " + row[0])
+							log.append("found " + row[0])
+							list_cfe.append({"name": row[0], "account": row[1]})
+						else:
+							print("error, supplier not unique " + row[0])
+							log.append("error, found <> 1 suppliers " + row[0])
+					else:
+						cfn += 1
+						print("new supplier " + row[0])
+						log.append("new supplier " + row[0])
+						list_cfn.append({"name": row[0], "account": row[1]})
+				except Exception as e:
+					print(e.args[0])
+					log.append(e.args)
+
+			for supplier in list_cfn:
+				supplier_doc = frappe.get_doc({
+						"doctype": "Supplier",
+						"supplier_name": supplier["name"],
+						"supplier_group": self.supplier_group,
+						"supplier_type": "Company"
+				})
+				supplier_doc.insert()
+			
+			supplier_list_all = frappe.get_list("Supplier", fields=["supplier_name", "name"] )
+			print("------")
+			pprint(supplier_list_all)
+			for s in supplier_list_all:
+				for e in list_all:
+					if s["supplier_name"] == e["name"]:
+						log.append("gefunden: " + e["name"] +" " + s["name"])
+						supp_doc = frappe.get_doc("Supplier", s["name"])
+						supp_doc.supplier_type = "Company"
+						if supp_doc.accounts:
+							pass #log.append("has account")
+						else:
+							log.append("account missing")
+							try:
+								sac = frappe.get_doc({
+														"doctype": "Account",
+														"company": self.company,
+														"parent_account": self.supplier_account,
+														"account_type": self.supplier_account_type,
+														"account_name": supp_doc.supplier_name,
+														"account_number": e["account"]
+														}).insert()
+							except Exception as e:
+								pass
+							supp_doc.append("accounts", {
+														"company": sac.company,
+														"account": sac.name
+														})
+							supp_doc.save()
+			no_acc = 0
+			for s in supplier_list_all:
+				supp_doc = frappe.get_doc("Supplier", s["name"])
+				if not supp_doc.accounts:
+					no_acc += 1
+					supp_doc.supplier_type = "Company"
+					print(s["name"] + " has still no account")
+					next_account = max(accounts) + 1
+					try:
+						sac = frappe.get_doc({
+													"doctype": "Account",
+													"company": self.company,
+													"parent_account": self.supplier_account,
+													"account_type": self.supplier_account_type,
+													"account_name": supp_doc.supplier_name,
+													"account_number": next_account
+													}).insert()
+						accounts.append(next_account)
+					except Exception as e:
+						pass
+					supp_doc.append("accounts", {
+												"company": sac.company,
+												"account": sac.name
+												})
+					supp_doc.save()
+
+
+
+
+
+
+						
+						
+			
+
+			
+
+
+
+
+
+
+			log.append(str(cff) + " Lines Proccesed")
+			log.append(str(cfn) + " new suppliers")
+			log.append(str(cfe) + " existing suppliers")
+
+			logtext = ""
+			for line in log:
+				logtext = logtext + str(line) + "<br>"
+			self.supplier_log = logtext
+						
+					
+		print("stats: cff: " + str(cff) + " cfe:  " + str(cfe))
+		#print(list_cfe)
+		#print(list_cfn)
+		print("max account: " + str(max(accounts)))
+		print("no acc: " + str(no_acc))
 	pass
 
